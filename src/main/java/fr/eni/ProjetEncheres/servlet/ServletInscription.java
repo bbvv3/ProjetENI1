06cb4aa3403b047ef2b191302207757fc43cfe9f
Victor Boisson <v.boisson97@gmail.com>
2022-07-19 11:26:30 +0200

    creation dossier bootstrap premiere JSP et Servlet

---
+++ src/main/java/fr/eni/ProjetEncheres/servlet/ServletInscription.java
@@ -0,0 +1,37 @@
+package fr.eni.ProjetEncheres.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class ServletInscription
+ */
+@WebServlet("/ServletInscription")
+public class ServletInscription extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Redirection vers la jsp Inscription.jsp
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/Inscription.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
