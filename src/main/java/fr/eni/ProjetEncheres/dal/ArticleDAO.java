c9de27ffd698c267ca46fc3e839a0521e0824fc0
auror <auror@host.docker.internal>
2022-07-21 17:05:57 +0200

    création ArticleDAO

---
+++ src/main/java/fr/eni/ProjetEncheres/dal/ArticleDAO.java
@@ -0,0 +1,19 @@
+package fr.eni.ProjetEncheres.dal;
+
+import java.util.List;
+
+import fr.eni.ProjetEncheres.bo.Article;
+import fr.eni.ProjetEncheres.bo.Categorie;
+
+public interface ArticleDAO extends DAO<Article>{
+	
+	
+	 //liste articles associée au mot clef
+
+	 List<Article> selectByMotCle(String mot_cle);
+	 
+	 List<Article> selectByCategorie(Categorie categorie);
+
+	//liste d'articles associée a la categorie
+	
+}
