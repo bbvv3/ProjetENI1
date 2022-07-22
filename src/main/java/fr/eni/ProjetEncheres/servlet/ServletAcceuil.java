51596cf9aceec603fa164e436bb03a4236ffbc37
Victor Boisson <v.boisson97@gmail.com>
2022-07-22 14:43:29 +0200

    mise en place du front deconnexion

--- src/main/java/fr/eni/ProjetEncheres/servlet/ServletAcceuil.java
+++ src/main/java/fr/eni/ProjetEncheres/servlet/ServletAcceuil.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class ServletAcceuil
@@ -22,7 +23,11 @@ public class ServletAcceuil extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		HttpSession session = request.getSession();
+		String deconnexion = request.getParameter("logout");
+		if(deconnexion == "1") {
+			session.invalidate();
+		}
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/index.jsp");
 		rd.forward(request, response);
 	}
