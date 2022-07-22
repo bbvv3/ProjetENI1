f883e119a102b053e949ee8c29a6040cc8213a37
auror <auror@host.docker.internal>
2022-07-22 15:19:04 +0200

    mise a jour article

---
+++ src/main/java/fr/eni/ProjetEncheres/bll/ArticleManager.java
@@ -0,0 +1,37 @@
+package fr.eni.ProjetEncheres.bll;
+
+import java.util.List;
+
+import fr.eni.ProjetEncheres.bo.Article;
+import fr.eni.ProjetEncheres.bo.Categorie;
+import fr.eni.ProjetEncheres.dal.DAOFactory;
+
+public class ArticleManager {
+	
+		//singleton
+		private static ArticleManager instance;
+
+		private ArticleManager() {}
+		
+		
+		public static ArticleManager getInstance() {
+			if(instance==null) {
+				instance = new ArticleManager();
+			}
+			return instance;
+		}
+		
+		
+		
+	
+		
+		public List<Article> selectBymotcle(String mot_cle){
+			return DAOFactory.getArticleDAO().selectByMotCle(mot_cle); 
+		}
+		
+		public List<Article> selectByCategorie(int identifiant){
+			return DAOFactory.getArticleDAO().selectByCategorie(identifiant);
+		}
+		
+		
+}
